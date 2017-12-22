7b15ac8255c4dd8bf85a1311e420d6aa302cb35c
dzelzs <mpanfilov@gmail.com>
2017-12-22 12:49:31 +0300

    implemented modulo

---
+++ include/modulo_int.h
@@ -0,0 +1,119 @@
+#pragma once
+#include <iostream>
+#include <limits>
+#include <type_traits>
+
+namespace olymp {
+namespace modulo {
+
+template<class T>
+constexpr bool is_unsigned = std::is_unsigned<T>::value;
+
+template<class T>
+constexpr bool is_signed = std::is_signed<T>::value;
+
+template <unsigned long long Modulo, class T = unsigned long long, class = void>
+class Int;
+
+template <unsigned long long Modulo, class T>
+class Int<Modulo, T, std::enable_if_t<is_unsigned<T>>> {
+    using Self = Int<Modulo, T>;
+
+public:
+    explicit Int(T init) : value(init % Modulo) {}
+
+    template <class U, std::enable_if_t<is_unsigned<U>> * = nullptr>
+    Self &operator+=(const U u) {
+        static_assert(std::numeric_limits<T>::max() / 2 >= Modulo,
+                      "Modulo is too large to work with this type of integer");
+        value += (u % Modulo);
+        value %= Modulo;
+        return *this;
+    }
+
+    template <class U, std::enable_if_t<is_signed<U>> * = nullptr>
+    Self &operator+=(U u) {
+        return this->operator+=(invert_negative(u));
+    }
+
+    template <class U, std::enable_if_t<is_signed<U>> * = nullptr>
+    Self &operator-=(const U u) {
+        return this->operator+=(U(-u));
+    }
+
+    template <class U, std::enable_if_t<is_unsigned<U>> * = nullptr>
+    Self &operator*=(const U u) {
+        static_assert(std::numeric_limits<T>::max() / Modulo >= Modulo,
+                      "Modulo is too large to work with this type of integer");
+        value *= (u % Modulo);
+        value %= Modulo;
+        return *this;
+    }
+
+    template <class U, std::enable_if_t<is_signed<U>> * = nullptr>
+    Self &operator*=(U u) {
+        return this->operator*=(invert_negative(u));
+    }
+
+    Self operator++() {
+        return (*this) += 1;
+    }
+
+    Self operator--() {
+        return (*this) -= 1;
+    }
+
+    bool operator<(Self other) const { return value < other.value; }
+    bool operator==(Self other) const { return value == other.value; }
+    bool operator<=(Self other) const { return *this < other || *this == other; }
+    bool operator>(Self other) const { return other < *this; }
+    bool operator!=(Self other) const { return not (*this == other); }
+    bool operator>=(Self other) const { return other <= *this; }
+
+    Self &operator+=(const Self other) { return this->operator+=(other.value); }
+    Self &operator-=(const Self other) { return this->operator-=(other.value); }
+    T get_value() const { return value; }
+
+private:
+    template<class U>
+    T invert_negative(U u) {
+        if (u < 0) {
+            u += Modulo + Modulo * (-u / Modulo);
+        }
+        return T(u);
+    }
+    T value;
+};
+
+template <unsigned long long Modulo, class T = unsigned long long>
+std::ostream &operator<<(std::ostream &s, const Int<Modulo, T> mi) {
+    s << mi.get_value();
+    return s;
+}
+
+template<size_t Modulo>
+struct multiply {
+    template <class LHS, class RHS>
+    auto operator()(LHS lhs, RHS rhs) const {
+        static_assert(is_unsigned<LHS> && is_unsigned<RHS>,
+                      "Multiplication routine works with unsigned integers only");
+        lhs %= Modulo;
+        rhs %= Modulo;
+        return (lhs * rhs) % Modulo;
+    }
+};
+
+template<size_t Modulo>
+struct add {
+    template<class LHS, class RHS>
+    auto operator()(LHS lhs, RHS rhs) const {
+        static_assert(is_unsigned<LHS> && is_unsigned<RHS>,
+                      "Addition routine works with unsigned integers only");
+        lhs %= Modulo;
+        rhs %= Modulo;
+        return (lhs + rhs) % Modulo;
+    }
+};
+
+} // namespace modulo
+} // namespace olymp
---
+++ tests/modulo_int.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <modulo_int.h>
+#include <gtest/gtest.h>
+
+namespace olymp {
+
+using TestI = modulo::Int<11, uint8_t>;
+const modulo::multiply<11> test_mult;
+const modulo::add<11> test_add;
+
+TEST(ModuloInt, Addition) {
+    {
+        TestI x(0);
+        x += 5;
+        ASSERT_EQ(5, x.get_value());
+        x += 5;
+        ASSERT_EQ(10, x.get_value());
+        x += 5;
+        ASSERT_EQ(4, x.get_value());
+    }
+    {
+        TestI x(0);
+        x += TestI(5);
+        ASSERT_EQ(5, x.get_value());
+        x += TestI(5);
+        ASSERT_EQ(10, x.get_value());
+        x += TestI(5);
+        ASSERT_EQ(4, x.get_value());
+    }
+    {
+        unsigned x = 7;
+        unsigned y = 5;
+        ASSERT_EQ(1, test_add(x, y));
+    }
+}
+
+TEST(ModuloInt, Subtraction) {
+    {
+        TestI x(0);
+        x -= 5;
+        ASSERT_EQ(6, x.get_value());
+        x -= 5;
+        ASSERT_EQ(1, x.get_value());
+        x -= 5;
+        ASSERT_EQ(7, x.get_value());
+    }
+    {
+        TestI x(0);
+        x -= 60;
+        ASSERT_EQ(6, x.get_value());
+        x -= 5;
+        ASSERT_EQ(1, x.get_value());
+        x -= 5;
+        ASSERT_EQ(7, x.get_value());
+    }
+    {
+        TestI x(0);
+        x += (-5);
+        ASSERT_EQ(6, x.get_value());
+        x += (-5);
+        ASSERT_EQ(1, x.get_value());
+        x += (-5);
+        ASSERT_EQ(7, x.get_value());
+    }
+}
+
+TEST(ModuloInt, Multiplication) {
+    {
+        TestI x(0);
+        x *= 2u;
+        ASSERT_EQ(0, x.get_value());
+        x += 1u;
+        x *= 2u;
+        ASSERT_EQ(2, x.get_value());
+        x *= 3u;
+        ASSERT_EQ(6, x.get_value());
+        x *= 3u;
+        ASSERT_EQ(7, x.get_value());
+    }
+    {
+        TestI x(2);
+        x *= -2;
+        ASSERT_EQ(7, x.get_value());
+    }
+    {
+        unsigned x = 4;
+        unsigned y = 5;
+        ASSERT_EQ(9, test_mult(x, y));
+    }
+}
+
+TEST(ModuloInt, Pre) {
+    {
+        TestI x(0);
+        ASSERT_EQ(1, (++x).get_value());
+        ASSERT_EQ(0, (--x).get_value());
+        ASSERT_EQ(10, (--x).get_value());
+        ASSERT_EQ(0, (++x).get_value());
+    }
+}
+
+TEST(ModuloInt, Compare) {
+    {
+        ASSERT_LT(TestI(1), TestI(2));
+        ASSERT_EQ(TestI(1), TestI(1));
+        ASSERT_LE(TestI(1), TestI(2));
+        ASSERT_LE(TestI(1), TestI(1));
+        ASSERT_GT(TestI(2), TestI(1));
+        ASSERT_NE(TestI(2), TestI(1));
+        ASSERT_GE(TestI(2), TestI(1));
+        ASSERT_GE(TestI(1), TestI(1));
+    }
+}
+
+} // namespace olymp
